555ee756278f1da93e74dd8ebaa59ba7cda3a9cb
Juliane Nagao <julianens@gmail.com>
2019-04-09 15:11:20 +0200

    chore: add babel-loader to webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
