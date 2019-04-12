36db828c79180845ced48bad71fd228776d24cf7
Juliane Nagao <julianens@gmail.com>
2019-04-12 13:21:13 +0200

    feat: wrap App component around BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './containers/App';
 import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
