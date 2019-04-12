a4d91e2d197b7c5aa13c50d656fd374510e8d948
Juliane Nagao <julianens@gmail.com>
2019-04-12 13:23:06 +0200

    feat: add base routes ListBooks SearchBooks NotFoundBooks to App

---
+++ src/components/NotFoundBooks.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFoundBooks = () => <div>NotFoundBooks component</div>;
+
+export default NotFoundBooks;
---
+++ src/components/SearchBooks.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SearchBooks = () => <div>SearchBooks component</div>;
+
+export default SearchBooks;
